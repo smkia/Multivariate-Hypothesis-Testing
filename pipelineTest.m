f97058d1c5f52adbc6eb83bd672812640cb20351
Seyed Mostafa Kia <m.kia83@gmail.com>
2013-11-19 15:42:59 +0100

    Neuro cloud test function

---
+++ pipelineTest.m
@@ -0,0 +1,33 @@
+function [] = pipelineTest(resultFileAddress)
+% Simulation Config
+cfgSim = [];
+cfgSim.trialNum = 100;
+cfgSim.channelNum = 274;
+cfgSim.timeNum = 50;
+cfgSim.freqNum = 45;
+cfgSim.affectedChannel = [208:217];
+cfgSim.sigmaVar = [4,7];
+cfgSim.effectSize = 11;
+cfgSim.timeVar = [21,35];
+cfgSim.freqVar = [15,29];
+% Preparing Targets
+targets = zeros(1,cfgSim.trialNum);
+targets(1,1:round(cfgSim.trialNum/2)) = 1;
+% Hierarchical Method config
+%cfgHrc.coefNum = 5;
+cfgHrc.criticalAlpha = 0.05;
+cfgHrc.iterations = 10000;
+%cfgHrc.featureExt = 'DCT';
+cfgHrc.featureExt = 'DCT';
+cfgSim.ampVar = [7,12];
+cfgHrc.MCPMethod = {'BF','BF','BF'};
+data_tf = [];
+[data_tf,mask,SNR] = simulatingData(cfgSim,data_tf);
+for j = 1 : 30
+    % Hierarchy test Bonferroni
+    cfgHrc.coefNum = j;
+    [hierarchyMask] = hierarchyTest(cfgHrc,data_tf,targets);
+    [sensitivityHierarchyBF(j),specificityHierarchyBF(j)] = testEvaluation(hierarchyMask,mask);
+    save(resultFileAddress,'SNR','sensitivityHierarchyBF','specificityHierarchyBF');
+    disp(strcat(num2str(j)));
+end
