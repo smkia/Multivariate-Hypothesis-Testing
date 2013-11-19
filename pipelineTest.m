279c647134d9b3ea2fe64e8a84f991540f0277ee
Seyed Mostafa Kia <m.kia83@gmail.com>
2013-11-19 17:38:10 +0100

    testPipeline improvement

--- pipelineTest.m
+++ pipelineTest.m
@@ -1,27 +1,28 @@
-function [] = pipelineTest(resultFileAddress)
-% Simulation Config
-cfgSim = [];
-cfgSim.trialNum = 100;
-cfgSim.channelNum = 274;
-cfgSim.timeNum = 50;
-cfgSim.freqNum = 45;
-cfgSim.affectedChannel = [208:217];
-cfgSim.sigmaVar = [4,7];
-cfgSim.effectSize = 11;
-cfgSim.timeVar = [21,35];
-cfgSim.freqVar = [15,29];
-% Preparing Targets
+function [] = pipelineTest(cfgFileAddress,resultFileAddress)
+load(cfgFileAddress);
+% % Simulation Config
+% cfgSim = [];
+% cfgSim.trialNum = 10;
+% cfgSim.channelNum = 274;
+% cfgSim.timeNum = 50;
+% cfgSim.freqNum = 45;
+% cfgSim.affectedChannel = [208:209];
+% cfgSim.sigmaVar = [4,7];
+% cfgSim.effectSize = 11;
+% cfgSim.timeVar = [21,35];
+% cfgSim.freqVar = [15,29];
+% % Preparing Targets
+% % Hierarchical Method config
+% %cfgHrc.coefNum = 5;
+% cfgHrc.criticalAlpha = 0.05;
+% cfgHrc.iterations = 10000;
+% %cfgHrc.featureExt = 'DCT';
+% cfgHrc.featureExt = 'DCT';
+% cfgSim.ampVar = [7,12];
+% cfgHrc.MCPMethod = {'BF','BF','BF'};
+data_tf = [];
 targets = zeros(1,cfgSim.trialNum);
 targets(1,1:round(cfgSim.trialNum/2)) = 1;
-% Hierarchical Method config
-%cfgHrc.coefNum = 5;
-cfgHrc.criticalAlpha = 0.05;
-cfgHrc.iterations = 10000;
-%cfgHrc.featureExt = 'DCT';
-cfgHrc.featureExt = 'DCT';
-cfgSim.ampVar = [7,12];
-cfgHrc.MCPMethod = {'BF','BF','BF'};
-data_tf = [];
 [data_tf,mask,SNR] = simulatingData(cfgSim,data_tf);
 for j = 1 : 30
     % Hierarchy test Bonferroni
